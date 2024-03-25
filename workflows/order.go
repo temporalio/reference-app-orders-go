802894a201323c4e65777e9d1d9d3436721b3f6c
Rob Holland <rob.holland@gmail.com>
2024-03-25 11:47:33 +0000

    Renaming from PR comments.

--- workflows/order.go
+++ workflows/order.go
@@ -6,37 +6,37 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
-type order struct {
-	ID          ordersapi.OrderID
-	fulfilments []fulfilment
-	shipments   []workflow.Future
+type orderImpl struct {
+	ID           ordersapi.OrderID
+	fulfillments []fulfillment
+	shipments    []workflow.Future
 }
 
-type fulfilment struct {
+type fulfillment struct {
 	Items []ordersapi.Item
 }
 
 func Order(ctx workflow.Context, input ordersapi.OrderInput) (ordersapi.OrderResult, error) {
-	return new(order).run(ctx, input)
+	return new(orderImpl).run(ctx, input)
 }
 
-func (o *order) run(ctx workflow.Context, order ordersapi.OrderInput) (ordersapi.OrderResult, error) {
+func (o *orderImpl) run(ctx workflow.Context, order ordersapi.OrderInput) (ordersapi.OrderResult, error) {
 	var result ordersapi.OrderResult
 
 	o.ID = order.ID
-	o.fulfil(order.Items)
+	o.fulfill(order.Items)
 	o.createShipments(ctx)
 
 	return result, o.waitForDeliveries(ctx)
 }
 
-func (o *order) fulfil(items []ordersapi.Item) {
+func (o *orderImpl) fulfill(items []ordersapi.Item) {
 	// Hard coded. Open discussion where this stub boundary should live.
 
 	// First item from one warehouse
-	o.fulfilments = append(
-		o.fulfilments,
-		fulfilment{
+	o.fulfillments = append(
+		o.fulfillments,
+		fulfillment{
 			Items: items[0:1],
 		},
 	)
@@ -45,17 +45,17 @@ func (o *order) fulfil(items []ordersapi.Item) {
 		return
 	}
 
-	// Second fulfilment with all other items
-	o.fulfilments = append(
-		o.fulfilments,
-		fulfilment{
+	// Second fulfillment with all other items
+	o.fulfillments = append(
+		o.fulfillments,
+		fulfillment{
 			Items: items[1 : len(items)-1],
 		},
 	)
 }
 
-func (o *order) createShipments(ctx workflow.Context) {
-	for i, f := range o.fulfilments {
+func (o *orderImpl) createShipments(ctx workflow.Context) {
+	for i, f := range o.fulfillments {
 		ctx = workflow.WithChildOptions(ctx,
 			workflow.ChildWorkflowOptions{
 				WorkflowID: shipmentapi.ShipmentWorkflowID(o.ID, i),
@@ -72,7 +72,7 @@ func (o *order) createShipments(ctx workflow.Context) {
 	}
 }
 
-func (o *order) waitForDeliveries(ctx workflow.Context) error {
+func (o *orderImpl) waitForDeliveries(ctx workflow.Context) error {
 	for _, d := range o.shipments {
 		err := d.Get(ctx, nil)
 		if err != nil {
