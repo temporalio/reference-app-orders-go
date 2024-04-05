30712a6bfbd45b95f11c291984e217df577cbfd5
Tom Wheeler <tom@temporal.io>
2024-04-05 09:43:58 -0500

    rewritten to test the result as a whole instead of individual fields

--- order/activities_test.go
+++ order/activities_test.go
@@ -28,17 +28,18 @@ func TestFulfillOrderOneItem(t *testing.T) {
 	var result order.FulfillOrderResult
 	require.NoError(t, future.Get(&result))
 
-	fulfillments := result.Fulfillments
-	require.Equal(t, 1, len(fulfillments))
+	expected := order.FulfillOrderResult{
+		Fulfillments: []order.Fulfillment{
+			{
+				Location: "Warehouse A",
+				Items: []order.Item{
+					{SKU: "Hiking Boots", Quantity: 2},
+				},
+			},
+		},
+	}
 
-	fulfillment := fulfillments[0]
-	require.Equal(t, "Warehouse A", fulfillment.Location)
-
-	require.Equal(t, 1, len(fulfillment.Items))
-
-	item := fulfillment.Items[0]
-	require.Equal(t, "Hiking Boots", item.SKU)
-	require.Equal(t, int32(2), item.Quantity)
+	require.Equal(t, expected, result)
 }
 
 func TestFulfillOrderTwoItems(t *testing.T) {
@@ -62,24 +63,22 @@ func TestFulfillOrderTwoItems(t *testing.T) {
 	var result order.FulfillOrderResult
 	require.NoError(t, future.Get(&result))
 
-	fulfillments := result.Fulfillments
-	require.Equal(t, 2, len(fulfillments))
+	expected := order.FulfillOrderResult{
+		Fulfillments: []order.Fulfillment{
+			{
+				Location: "Warehouse A",
+				Items: []order.Item{
+					{SKU: "Hiking Boots", Quantity: 2},
+				},
+			},
+			{
+				Location: "Warehouse B",
+				Items: []order.Item{
+					{SKU: "Tennis Shoes", Quantity: 1},
+				},
+			},
+		},
+	}
 
-	fulfillmentA := fulfillments[0]
-	require.Equal(t, "Warehouse A", fulfillmentA.Location)
-
-	require.Equal(t, 1, len(fulfillmentA.Items))
-
-	itemOne := fulfillmentA.Items[0]
-	require.Equal(t, "Hiking Boots", itemOne.SKU)
-	require.Equal(t, int32(2), itemOne.Quantity)
-
-	fulfillmentB := fulfillments[1]
-	require.Equal(t, "Warehouse B", fulfillmentB.Location)
-
-	require.Equal(t, 1, len(fulfillmentB.Items))
-
-	itemTwo := fulfillmentB.Items[0]
-	require.Equal(t, "Tennis Shoes", itemTwo.SKU)
-	require.Equal(t, int32(1), itemTwo.Quantity)
+	require.Equal(t, expected, result)
 }
