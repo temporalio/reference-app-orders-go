9dcb8ec4638e6d6c4dec5de6945c2191799c4a65
Tom Wheeler <tom@temporal.io>
2024-04-10 15:47:14 -0500

    Removed code that pertained to non-standard TLS setups

--- internal/temporal/client_config.go
+++ internal/temporal/client_config.go
@@ -2,10 +2,8 @@ package temporal
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"fmt"
 	"os"
-	"strconv"
 
 	"go.temporal.io/sdk/client"
 )
@@ -18,9 +16,6 @@ import (
 //	TEMPORAL_NAMESPACE: Namespace to be used by the Client
 //	TEMPORAL_TLS_CERT: Path to the x509 certificate
 //	TEMPORAL_TLS_KEY: Path to the private certificate key
-//	TEMPORAL_TLS_CA: Path to the server CA certificate
-//	TEMPORAL_TLS_DISABLE_HOST_VERIFICATION: Disables TLS host name verification
-//	TEMPORAL_TLS_SERVER_NAME: Overrides target TLS server name
 //
 // If none of these environment variables are set, this will return a
 // ClientOptions instance configured to connect to port 7233 on the
@@ -36,7 +31,6 @@ func CreateClientOptionsFromEnv() (client.Options, error) {
 	clientOpts.HostPort = getEnvWithFallback("TEMPORAL_ADDRESS", "localhost:7233")
 	clientOpts.Namespace = getEnvWithFallback("TEMPORAL_NAMESPACE", "default")
 
-
 	// Other TLS-related parameters are ignored unless the cert and
 	// key paths are specified.
 	if isSet("TEMPORAL_TLS_CERT") && isSet("TEMPORAL_TLS_KEY") {
@@ -51,34 +45,6 @@ func CreateClientOptionsFromEnv() (client.Options, error) {
 
 		var tlsConfig = tls.Config{}
 		tlsConfig.Certificates = []tls.Certificate{clientCert}
-
-		if isSet("TEMPORAL_TLS_CA") {
-			caCertPath := os.Getenv("TEMPORAL_TLS_CA")
-
-			caCertPool := x509.NewCertPool()
-			data, err := os.ReadFile(caCertPath)
-			if err != nil {
-				return client.Options{}, fmt.Errorf("failed to read CA cert: %w", err)
-			} else if !caCertPool.AppendCertsFromPEM(data) {
-				return client.Options{}, fmt.Errorf("failed to append CA cert")
-			}
-
-			tlsConfig.RootCAs = caCertPool
-		}
-
-		if isSet("TEMPORAL_TLS_DISABLE_HOST_VERIFICATION") {
-			strVal := os.Getenv("TEMPORAL_TLS_DISABLE_HOST_VERIFICATION")
-			disableHostVerification, err := strconv.ParseBool(strVal)
-			if err != nil {
-				msg := "parse failed for '%s' in TEMPORAL_TLS_DISABLE_HOST_VERIFICATION"
-				return client.Options{}, fmt.Errorf(msg, strVal)
-			}
-			tlsConfig.InsecureSkipVerify = disableHostVerification
-		}
-
-		if isSet("TEMPORAL_TLS_SERVER_NAME") {
-			tlsConfig.ServerName = os.Getenv("TEMPORAL_TLS_SERVER_NAME")
-		}
 
 		clientOpts.ConnectionOptions = client.ConnectionOptions{
 			TLS: &tlsConfig,
