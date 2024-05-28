9599d28b35143ad5db176e287290b917a71d21d7
Rob Holland <rob@temporal.io>
2024-05-28 16:04:48 +0100

    Remove stray SQLite reference.

--- app/billing/api_test.go
+++ app/billing/api_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/temporalio/orders-reference-app-go/app/billing"
-	_ "modernc.org/sqlite"
 )
 
 func TestChargeWorkflowID(t *testing.T) {
