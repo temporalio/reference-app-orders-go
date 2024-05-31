1c9258076a13c32d848c9fddae19a142fd804d5d
Alex Tideman <alex.tideman@gmail.com>
2024-05-31 10:27:48 -0500

    Remove nav links

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -14,12 +14,6 @@
 	<header>
 		<nav>
 			<Logo />
-			<div class="action">
-				<div class="links">
-					<a href="/orders" class:active={$page.url.pathname.includes('orders')}>Orders</a>
-					<a href="/shipments" class:active={$page.url.pathname.includes('shipments')}>Shipments</a>
-				</div>
-			</div>
 		</nav>
 	</header>
 	<main>
@@ -40,33 +34,12 @@
 		justify-content: end;
 	}
 
-	.action {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-	}
-
 	nav {
 		margin-bottom: 0;
 		display: flex;
 		justify-content: space-between;
 		width: 100%;
 		height: 100px;
-	}
-
-	nav a {
-		letter-spacing: -1px;
-		font-weight: 600;
-		text-transform: uppercase;
-	}
-
-	.links {
-		display: flex;
-		gap: 1rem;
-	}
-
-	.active {
-		text-decoration: underline;
 	}
 
 	main {
